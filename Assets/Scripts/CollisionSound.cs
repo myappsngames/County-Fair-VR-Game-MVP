5cdc3a6557ac50d2d031815c54c690a311829e61
myappsngames <62448812+myappsngames@users.noreply.github.com>
2023-10-20 10:57:25 -0700

    Added Audio to Test Your Strength & Ring Toss Booths

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+// Require AudioSource to use this script
+[RequireComponent (typeof(AudioSource))]
+
+public class CollisionSound : MonoBehaviour
+{
+    // field to change tags
+    [SerializeField] private string objectTag;
+
+    // field to control min and max pitch of sound
+    [SerializeField] float minPitch = 0.9f;
+    [SerializeField] float maxPitch = 1.1f;
+
+    // variable for audio source
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        // get audio source
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // event for detecting collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(objectTag))
+        {
+            // varies the pitch
+            _audioSource.pitch = Random.Range(minPitch, maxPitch);
+            // PlayOneShot allows sound clip to finish
+            _audioSource.PlayOneShot(_audioSource.clip);
+
+        }
+    }
+}
